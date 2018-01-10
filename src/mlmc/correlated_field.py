32b59def0d300be19d46ab81b3d96b4f00c06f8f
Klara S <klara.steklova@gmail.com>
2018-01-10 12:23:28 +0000

    correlated_field.py
    changed cov_matrix computation

--- src/mlmc/correlated_field.py
+++ src/mlmc/correlated_field.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1252 -*-
 import numpy as np
 import numpy.linalg as la
 import scipy as sp
@@ -121,7 +122,7 @@ class SpatialCorrelatedField(object):
         length_srq_mat = np.sum(np.inner(diff_mat, self.correlation_tensor) * diff_mat, axis =-1)
 
         corr_exp = self.correlation_exponent / 2.0
-        exp_scale = - 1.0 / self.correlation_exponent
+        exp_scale = - 1.0 /(self._max_corr_length)  # was missing corr length
         self.cov_mat = np.exp( exp_scale*length_srq_mat**self.correlation_exponent  )
         return self.cov_mat
 
