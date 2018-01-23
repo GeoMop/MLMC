81ff35e4c44fe2faae00fa77ec1e1febd698208f
Klara S <klara.steklova@gmail.com>
2018-01-23 13:44:00 +0000

    changed pathways

--- test/driver_mc.py
+++ test/driver_mc.py
@@ -1,21 +1,18 @@
-import MCwork
 from MCwork import FlowMC
-#import importlib
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats.mstats import mquantiles
 
-#importlib.import_module('MCwork')
-fileDir = os.path.dirname(os.path.realpath('__file__'))
+fileDir = os.path.dirname(os.path.realpath(__file__))
 
-yaml_path = '02_mysquare.yaml'  #Documents\Intec\PythonScripts\\MonteCarlo\\Flow_02_test\\
-mesh_path = 'Flow_02_test/square_mesh.msh'
+yaml_path = '02_mysquare.yaml' 
+mesh_path = 'square_mesh.msh'
 
 run2      = FlowMC(yaml_path, mesh_path)
 
-n_realzs  = 4 
+n_realzs  = 400 
 f         = np.zeros(n_realzs,)
 
 for i in range(n_realzs):
