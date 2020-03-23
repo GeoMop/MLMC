d11baf8ed88d3f8a11e933763fff7009eae71d55
Martin Špetlík <martin.spetlik@tul.cz>
2020-03-23 16:14:40 +0100

    readme

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 
 long_description='%s\n%s' % (
-    re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+    re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md')),
     re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGES.rst'))
 )
 
