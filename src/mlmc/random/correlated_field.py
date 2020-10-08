e0d8684eb238b38dea3bdd170659e027c1249fd8
martinspetlik <spetlik.martin@eseznam.cz>
2020-10-08 13:45:18 +0200

    gstools field log

--- src/mlmc/random/correlated_field.py
+++ src/mlmc/random/correlated_field.py
@@ -501,7 +501,7 @@ class SpatialCorrelatedField(RandomFieldBase):
 
 class GSToolsSpatialCorrelatedField(RandomFieldBase):
 
-    def __init__(self, model, mode_no=1000):
+    def __init__(self, model, mode_no=1000, log=False):
         """
         :param model: instance of covariance model class, which parent is gstools.covmodel.CovModel
         :param mode_no: number of Fourier modes, default: 1000 as in gstools package
@@ -510,6 +510,7 @@ class GSToolsSpatialCorrelatedField(RandomFieldBase):
         self.mode_no = mode_no
         self.srf = gstools.SRF(model, mode_no=mode_no)
         self.dim = model.dim
+        self.log = log
 
     def change_srf(self, seed):
         """
@@ -546,7 +547,9 @@ class GSToolsSpatialCorrelatedField(RandomFieldBase):
         """
         :return: Random field evaluated in points given by 'set_points'
         """
-        return self.sigma * self.random_field() + self.mu
+        if not self.log:
+            return self.sigma * self.random_field() + self.mu
+        return np.exp(self.sigma * self.random_field() + self.mu)
 
 
 class FourierSpatialCorrelatedField(RandomFieldBase):
