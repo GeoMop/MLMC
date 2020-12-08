b740ddea7942bde372a25f5b33174fd6752a29d4
martinspetlik <martin.spetlik>
2020-12-08 13:35:30 +0100

    bootstrap

--- src/mlmc/quantity_spec.py
+++ src/mlmc/quantity_spec.py
@@ -11,7 +11,7 @@ class QuantitySpec:
     locations: Union[List[str], List[Tuple[float, float, float]]]
 
 
-@attr.s(auto_attribs=True)
+@attr.s(auto_attribs=True, eq=False)  # eq=False allows custom __hash__ and __eq__
 class ChunkSpec:
     level_id: int
     # Level identifier
@@ -21,3 +21,12 @@ class ChunkSpec:
     # Number of samples which we want to retrieve
     chunk_size: int = 512000000
     # Chunk size in bytes in decimal, determines number of samples in chunk
+
+    def __hash__(self):
+        return hash((self.level_id, self.chunk_id, self.n_samples, self.chunk_size))
+
+    def __eq__(self, other):
+        return (self.level_id, self.chunk_id, self.n_samples, self.chunk_size) == \
+               (other.level_id, other.chunk_id, other.n_samples, other.chunk_size)
+
+
