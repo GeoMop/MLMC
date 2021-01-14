818c2404f42b0d79f7395a6d670e0364a2cfabf1
martinspetlik <martin.spetlik>
2021-01-14 09:57:15 +0100

    quantity spec frozen

--- src/mlmc/quantity_spec.py
+++ src/mlmc/quantity_spec.py
@@ -1,21 +1,14 @@
 import attr
-import numpy as np
 from typing import List, Tuple, Union
 
 
-@attr.s(auto_attribs=True)
+@attr.s(auto_attribs=True, frozen=True)
 class QuantitySpec:
     name: str
     unit: str
     shape: Tuple[int, int]
     times: List[float]
     locations: Union[List[str], List[Tuple[float, float, float]]]
-
-    def __eq__(self, other):
-        if (self.name, self.unit) == (other.name, other.unit) and np.allclose(self.shape, other.shape) and np.allclose(self.times, other.times)\
-                and not (set(self.locations) - set(other.locations)):
-            return True
-        return False
 
 
 @attr.s(auto_attribs=True, frozen=True)
@@ -28,4 +21,3 @@ class ChunkSpec:
     # Number of samples which we want to retrieve
     chunk_size: int = 512000000
     # Chunk size in bytes in decimal, determines number of samples in chunk
-
