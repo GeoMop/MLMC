287487f2869f0bd6cf5eeba9780def82e0cde2e3
martinspetlik <martin.spetlik>
2022-01-28 13:00:55 +0100

    docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "{}, Jan Březina, Martin Špetlík".format(curr_year)
 author = "Jan Březina, Martin Špetlík"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -83,7 +83,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # autodoc_default_options = {
 #     'members': True,
