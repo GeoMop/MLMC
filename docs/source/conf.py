45be7ed1177c0b3ee68bd6d08eae747f3adcf858
martinspetlik <martin.spetlik>
2022-01-28 13:08:40 +0100

    doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 import datetime
+sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
@@ -83,7 +84,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # autodoc_default_options = {
 #     'members': True,
