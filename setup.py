0c708498a1d4463f019b04994bfe7e13490233f3
martinspetlik <martin.spetlik>
2025-10-19 14:25:02 +0200

    fix ruamel.yaml version

--- setup.py
+++ setup.py
@@ -61,5 +61,5 @@ setup(
     # include automatically all files in the template MANIFEST.in
     include_package_data=True,
     zip_safe=False,
-    install_requires=['numpy', 'scipy', 'scikit-learn', 'h5py>=3.1.0', 'ruamel.yaml==0.17.26', 'attrs', 'gstools', 'memoization'],
+    install_requires=['numpy', 'scipy', 'scikit-learn', 'h5py>=3.1.0', 'ruamel.yaml', 'attrs', 'gstools', 'memoization'],
 )
